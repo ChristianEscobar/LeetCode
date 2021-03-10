ea094182382039c77c5f6c537cb1cc3d1f5836d0
Christian Escobar <crs.escobar@gmail.com>
2021-03-09 19:43:01 -0800

    checking in unfinished solution

---
+++ DynamicProgramming/divisorGame.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function(N) {
+
+	function findX(num) {
+		const x = num - 1;
+		if(x < 0) {
+			return -1;
+		}
+
+		if(x < N && x >= 0) {
+			if(N % x === 0) {
+				return x;
+			}
+		}
+
+		return findX(x);
+	}
+
+	let doesAliceWin = false;
+	let turnCount = 1;
+	
+	while(N > 0) {
+		const num = findX(N);
+		
+		// Check if it is Alice's turn, her turns will be odd numbers
+		if(num === -1) {
+			if(turnCount % 2 === 0) {
+				doesAliceWin = true;
+			} else {
+				doesAliceWin = false;
+			}
+		} else {
+			if(turnCount % 2 === 0) {
+				doesAliceWin = false;
+			} else {
+				doesAliceWin = true;
+			}
+		}
+
+		N = Math.sign(num) * (N - num);
+		turnCount++;
+	}
+
+
+	return doesAliceWin;
+
+};
+
+console.log(divisorGame(3));
