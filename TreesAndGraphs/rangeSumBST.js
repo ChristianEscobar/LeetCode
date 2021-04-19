7de46fc288178148e5836cccb6c5bfd30bcce494
Christian Escobar <crs.escobar@gmail.com>
2021-04-19 16:35:52 -0700

    linter changes

--- TreesAndGraphs/rangeSumBST.js
+++ TreesAndGraphs/rangeSumBST.js
@@ -6,40 +6,40 @@ const { createTree } = require('./utils/utils');
  * @param {number} high
  * @return {number}
  */
-var rangeSumBST = function(root, low, high) {
-	return traverse(root, low, high, 0);
+const rangeSumBST = function (root, low, high) {
+  return traverse(root, low, high, 0);
 };
 
-var traverse = function(root, low, high, sum) {
-	if(root === null) {
-		return sum;
-	}
+const traverse = function (root, low, high, sum) {
+  if (root === null) {
+    return sum;
+  }
 
-	if(isInRange(root.val, low, high)) {
-		sum += root.val;
-	}
+  if (isInRange(root.val, low, high)) {
+    sum += root.val;
+  }
 
-	if(root.val >= low) {
-		sum = traverse(root.left, low, high, sum);
-	}
-	
-	if(root.val < high) {
-		sum = traverse(root.right, low, high, sum)
-	}
+  if (root.val >= low) {
+    sum = traverse(root.left, low, high, sum);
+  }
 
-	return sum;
-}
+  if (root.val < high) {
+    sum = traverse(root.right, low, high, sum);
+  }
 
-var isInRange = function(value, low, high) {
-	if(value >= low && value <= high) {
-		return true;
-	}
+  return sum;
+};
 
-	return false;
-}
+const isInRange = function (value, low, high) {
+  if (value >= low && value <= high) {
+    return true;
+  }
 
-const root1 = createTree([10,5,15,3,7,null,18]);
+  return false;
+};
+
+const root1 = createTree([10, 5, 15, 3, 7, null, 18]);
 console.log(rangeSumBST(root1, 7, 15));
 
-const root2 = createTree([10,5,15,3,7,13,18,1,null,6]);
+const root2 = createTree([10, 5, 15, 3, 7, 13, 18, 1, null, 6]);
 console.log(rangeSumBST(root2, 6, 10));
