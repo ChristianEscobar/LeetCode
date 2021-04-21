25d14b3aed0d35477f45998fb1fc6c6937ba923e
Christian Escobar <crs.escobar@gmail.com>
2021-04-21 08:12:23 -0700

    linter changes

--- TreesAndGraphs/utils/utils.js
+++ TreesAndGraphs/utils/utils.js
@@ -1,19 +1,19 @@
 /**
  * Definition for a binary tree node.
- */ 
+ */
 function TreeNode(val, left, right) {
-	this.val = (val===undefined ? 0 : val)
-	this.left = (left===undefined ? null : left)
-	this.right = (right===undefined ? null : right)
+  this.val = (val === undefined ? 0 : val);
+  this.left = (left === undefined ? null : left);
+  this.right = (right === undefined ? null : right);
 }
 
 /**
  * Definition for a Node.
  */
 function Node(val, children) {
-	this.val = val;
+  this.val = val;
  	this.children = children;
-};
+}
 
 /**
  * Definition for a Node.
@@ -21,99 +21,96 @@ function Node(val, children) {
 function NaryNode(val, children) {
  	this.val = val;
  	this.children = children;
- };
-
-const createTree = function(array) {
-	if(array.length === 0) {
-		return null;
-	}
-
-	let root = null;
-	let leftChildPos = 1;
-	let rightChildPos = leftChildPos + 1;
-	
-	for(let i=0; i < array.length; i++) {
-		if(array[i]) {
-			array[i] = new TreeNode(array[i]);
-		}
-	}
-
-
-	for(let i=0; i < array.length; i++) {
-		let node = array[i];
-		if(node) {
-			// Go get left and right child
-			let lChild = array[leftChildPos] ? array[leftChildPos] : null;
-			let rChild = array[rightChildPos] ? array[rightChildPos] : null;
-			node.left = lChild;
-			node.right = rChild;
-
-			if(i === 0) {
-				root = node;
-			}
-
-			leftChildPos +=2;
-			rightChildPos = leftChildPos + 1;
-
-		}
-	}
-
-	return root;
 }
 
-const createNaryTree = function(array) {
-	
-}
+const createTree = function (array) {
+  if (array.length === 0) {
+    return null;
+  }
 
-const preorderTraversal = function(root)
- {
+  let root = null;
+  let leftChildPos = 1;
+  let rightChildPos = leftChildPos + 1;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]) {
+      array[i] = new TreeNode(array[i]);
+    }
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    const node = array[i];
+    if (node) {
+      // Go get left and right child
+      const lChild = array[leftChildPos] ? array[leftChildPos] : null;
+      const rChild = array[rightChildPos] ? array[rightChildPos] : null;
+      node.left = lChild;
+      node.right = rChild;
+
+      if (i === 0) {
+        root = node;
+      }
+
+      leftChildPos += 2;
+      rightChildPos = leftChildPos + 1;
+    }
+  }
+
+  return root;
+};
+
+const createNaryTree = function (array) {
+
+};
+
+const preorderTraversal = function (root) {
 	 console.log(root.val);
 	 preorderTraversal(root.left);
 	 preorderTraversal(root.right);
- }
-const inorderTraversal = function(root) {
-	if(root === null) {
-		return;
-	}
+};
+const inorderTraversal = function (root) {
+  if (root === null) {
+    return;
+  }
 
-	inorderTraversal(root.left);
-	console.log(root.val);
-	inorderTraversal(root.right);
-}
+  inorderTraversal(root.left);
+  console.log(root.val);
+  inorderTraversal(root.right);
+};
 
-const postorderTraversal = function(root) {
-	postorderTraversal(root.left);
-	postorderTraversal(root.right);
-	console.log(root.val);
-}
+const postorderTraversal = function (root) {
+  postorderTraversal(root.left);
+  postorderTraversal(root.right);
+  console.log(root.val);
+};
 
-const levelOrderTraversal = function(root) {
-	if(root === null) {
-		return null;
-	}
+const levelOrderTraversal = function (root) {
+  if (root === null) {
+    return null;
+  }
 
-	const queue = [];
-	queue.push(root);
-	while(queue.length > 0) {
-		const node = queue.shift();
-		console.log(node.val);
-		if(node.left) {
-			queue.push(node.left);
-		}
-		
-		if(node.right) {
-			queue.push(node.right);
-		}
-	}
-}
+  const queue = [];
+  queue.push(root);
+  while (queue.length > 0) {
+    const node = queue.shift();
+    console.log(node.val);
+    if (node.left) {
+      queue.push(node.left);
+    }
+
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
+};
 
 module.exports = {
-	createTree,
-	TreeNode,
-	Node,
-	NaryNode,
-	preorderTraversal,
-	inorderTraversal,
-	postorderTraversal,
-	levelOrderTraversal,
-}
+  createTree,
+  TreeNode,
+  Node,
+  NaryNode,
+  preorderTraversal,
+  inorderTraversal,
+  postorderTraversal,
+  levelOrderTraversal,
+};
