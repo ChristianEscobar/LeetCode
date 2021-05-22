2f057c002538fbe7677adf6812bc059048e85e41
Christian Escobar <crs.escobar@gmail.com>
2021-05-21 21:00:17 -0700

    checking in latest changes

--- Strings/lengthOfLongestSubstring.js
+++ Strings/lengthOfLongestSubstring.js
@@ -1,22 +1,28 @@
-const lengthOfLongestSubstring = function(s) {
-	if(s.length === 0) {
-			return 0;
-	}
-	const sArray = s.split('');
-	const substringBuilder = [];
-	let longestLength = Number.MIN_SAFE_INTEGER;
-	for(let i = 0; i < sArray.length; i++) {
-			const char = sArray[i];
-			if(substringBuilder.includes(char)) {
-					let charIndex = substringBuilder.indexOf(char);
-					substringBuilder.splice(charIndex, 1);
-			}
-			
-			substringBuilder.push(char);
-			longestLength = Math.max(longestLength, substringBuilder.length);
-	}
-	
-	return substringBuilder.length;
+const lengthOfLongestSubstring = function (s) {
+  if (s.length === 0) {
+    return 0;
+  }
+  const sArray = s.split('');
+  const substringBuilder = [];
+  let longestLength = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < sArray.length; i += 1) {
+    const char = sArray[i];
+    while (substringBuilder.includes(char)) {
+      substringBuilder.shift();
+    }
+    // if (substringBuilder.includes(char)) {
+    //   // const charIndex = substringBuilder.indexOf(char);
+    //   // Remove character from substring
+    //   // substringBuilder.splice(charIndex, 1);
+    // }
+
+    substringBuilder.push(char);
+    longestLength = Math.max(longestLength, substringBuilder.length);
+  }
+
+  return longestLength;
 };
 
-console.log(lengthOfLongestSubstring('pwwkew'));
+console.assert(lengthOfLongestSubstring('abcabcbb') === 3, 'Expected 3 for abcabcbb');
+console.assert(lengthOfLongestSubstring('bbbbb') === 1, 'Expected 1 for bbbbb');
+console.assert(lengthOfLongestSubstring('pwwkew') === 3, 'Expected 3 for pwwkew');
